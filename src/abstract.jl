b6c7ff0974039ebc7c9ec67eb7510308ba9d5580
Arda Aytekin <aytekin@protonmail.com>
2016-06-03 17:47:24 +0200

    Modify abstract.jl
    Modified abstract.jl to include `series` and `parallel` catch-all
    functions. Also, added a TODO note for checking later on the efficiency.

--- src/abstract.jl
+++ src/abstract.jl
@@ -11,7 +11,15 @@ abstract DiscreteSisoTf   <: DiscreteTf
 abstract ContinuousSs     <: StateSpace
 abstract DiscreteSs       <: StateSpace
 
+# TODO: When everything is finished, try to write catch-all functions with a
+#       parametric style to see the difference, if any! Most likely, parametric
+#       version is better (more efficient), i.e.,
+#
+#       +{T1<:LtiSystem, T2<:LtiSystem}(s1::T1, s2::T2) = +(promote(s1, s2)...)
 +(sys1::LtiSystem, sys2::LtiSystem) = +(promote(sys1, sys2)...)
 -(sys1::LtiSystem, sys2::LtiSystem) = -(promote(sys1, sys2)...)
 *(sys1::LtiSystem, sys2::LtiSystem) = *(promote(sys1, sys2)...)
 /(sys1::LtiSystem, sys2::LtiSystem) = /(promote(sys1, sys2)...)
+
+series(sys1::LtiSystem, sys::LtiSystem)   = *(promote(sys1, sys2)...)
+parallel(sys1::LtiSystem, sys::LtiSystem) = +(promote(sys1, sys2)...)
